397ae5d7b65565b73525718db4e14f9fdbd61f3c
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2022-07-11 08:34:40 +0100

    WIP: Logic re-inserted, tests updated

--- dict2dot/__init__.py
+++ dict2dot/__init__.py
@@ -22,50 +22,57 @@ class Dict2Dot(dict):
     Dict2Dot class: "the main class"
 
     Arguments:
-        (dictionary, optional): A pre-existent dictionary may be passed
+        (dictionary or list, optional): A pre-existent dictionary may be passed
     """
-    def __init__(self, obj: (dict, list) = None, depth: int = 0, debug: bool = True):  # todo: debug defaults to False
-        self.log = Logger(log_level='DEBUG' if debug else 'WARNING').get_logger()
+    def __init__(self, obj: (dict, list) = None, depth: int = 0, debug: bool = False):  # todo: debug defaults to False
+        self._log = Logger(log_level='DEBUG' if debug else 'WARNING').get_logger()
+        self._depth = depth
 
-        _id = int(str(id(self))[-3:])
-        self.log.debug('{}{} depth {}, id {} for {}: {}'.format(
-            "  " * depth, self.__class__.__name__, depth, _id, type(obj).__name__.upper(), obj
+        self._id = int(str(id(self))[-3:])
+        self._log.debug('{}{} depth {}, id {} for {}: {}'.format(
+            "  " * depth, self.__class__.__name__, depth, self._id, type(obj).__name__.upper(), obj
         ))
 
         clone = obj
         if isinstance(obj, (dict, list)):
-            clone = obj.copy()
+            clone = obj.copy()  # need to keep original
             if isinstance(clone, dict):
                 super().__init__(clone.copy())
 
-        self.log.debug(f'{"  " * depth}Request object set')
+        self._log.debug(f'{"  " * depth}Request object set')
         self._set(clone, depth)
 
     def __getattr__(self, key):
         return self[key]
 
     def _set(self, obj, depth: int = 0):
-        self.log.debug(f'{"  " * depth}Start {type(obj).__name__.upper()} object parse')
+        self._log.debug(f'{"  " * depth}Start {type(obj).__name__.upper()} object parse')
 
         # keys = list(obj.keys()) if isinstance(obj, dict) else range(len(obj)) if isinstance(obj, list) else []
         keys = obj.items() if isinstance(obj, dict) else enumerate(obj) if isinstance(obj, list) else None
 
         if not keys:
-            self.log.debug(f'{"  " * depth}Object has NO keys: MAY need set')
+            self._log.debug(f'{"  " * depth}Object has NO keys: MAY need set')
 
         else:
-            self.log.debug(f'{"  " * depth}Start LOOP over keys {keys}')
+            self._log.debug(f'{"  " * depth}Start LOOP over keys {keys}')
 
             for key, value in keys:
-                self.log.debug(f'{"  " * depth}Start SET key {key}')
-                msg = '{}{} has {} as value type'.format("  " * depth, key, type(value).__name__.upper())
-                # todo: JUST GOT A MOUSE FREEZE WITH THE MAIN LOGIC SELECTED
-                # todo: AFTER REBOOTING THE IDE, LOGIC WAS GONE
-                # todo: NEED TO CREATE IT AGAIN
+                self._log.debug(f'{"  " * depth}Start SET key {key}')
+                msg = '{}{} has {} as value type -- '.format("  " * depth, key, type(value).__name__.upper())
 
-                self.log.debug(f'{"  " * depth}Ended key {key} set')
+                if isinstance(value, (dict, list)):
+                    msg += 'RECURSE'
+                    self._log.debug(msg)
+                    self[key] = Dict2Dot(value, depth + 1)
 
-        self.log.debug(f'{"  " * depth}Ended object {type(obj).__name__.upper()} parse')
+                else:
+                    msg += 'SET {} as {} in instance {}'.format(key, value, self._id)
+                    self._log.debug(msg)
+                    self[key] = value
+                    self._log.debug(f'{"  " * depth}Ended key {key} set')
+
+        self._log.debug(f'{"  " * depth}Ended object {type(obj).__name__.upper()} parse')
 
     def dict(self) -> dict:
         return loads('{}'.format(super().__repr__().replace("'", '"')))
@@ -88,4 +95,5 @@ class Dict2Dot(dict):
         return super().__repr__()
 
     def __repr__(self) -> str:
-        return '<{}.{} {}>'.format(self.__module__, self.__class__.__name__, super().__repr__())
+        name = '{}.{}'.format(self.__module__, self.__class__.__name__) if not self._depth else self.__class__.__name__
+        return '<{} {}>'.format(name, super().__repr__())
